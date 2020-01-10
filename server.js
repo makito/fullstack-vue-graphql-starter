edaae463b55f8761ff8593531fd206b05fb1de34
Ivan Orekhov <i.orehov@mcplat.ru>
2020-01-10 12:54:01 +0300

    add cloud mongodb connection

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const { ApolloServer, gql } = require('apollo-server');
+const mongoose = require('mongoose');
+require('dotenv').config({ path: 'variables.env' });
 
-const todos = [
-  { task: 'Wash car', completed: false },
-  { task: 'Clean room', completed: true },
-];
+mongoose
+  .connect(
+    process.env.MONGO_URI,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  )
+  .then(() => console.info('DB connected'))
+  .catch(err => console.error(err));
 
 const typeDefs = gql`
 
@@ -16,28 +24,16 @@ const typeDefs = gql`
     getTodos: [Todo]
   }
 
-  type Mutation {
-    addTodo(task: String, completed: Boolean): Todo
-  }
-
 `;
 
 const resolvers = {
   Query: {
     getTodos: () => todos
   },
-  Mutation: {
-    addTodo: (_, { task, completed }) => {
-      const todo = { task, completed };
-      todos.push(todo);
-      return todo;
-    }
-  }
 };
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers
+  typeDefs
 });
 
 server.listen().then(({ url }) => console.log(`server listening on ${url}`));
