e4fd5e8922ba51af085387fe0847ec2d49427749
Ivan Orekhov <i.orehov@mcplat.ru>
2020-01-10 12:10:13 +0300

    add graphql mutation for add task

--- server.js
+++ server.js
@@ -16,11 +16,22 @@ const typeDefs = gql`
     getTodos: [Todo]
   }
 
+  type Mutation {
+    addTodo(task: String, completed: Boolean): Todo
+  }
+
 `;
 
 const resolvers = {
   Query: {
     getTodos: () => todos
+  },
+  Mutation: {
+    addTodo: (_, { task, completed }) => {
+      const todo = { task, completed };
+      todos.push(todo);
+      return todo;
+    }
   }
 };
 
