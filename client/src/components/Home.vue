fb5f3aba0fb67fa0947a58692ad717043b624654
Ivan Orekhov <makito82@gmail.com>
2020-01-12 14:21:01 +0300

    add load posts on home page by graphql

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,9 +1,39 @@
 <template>
-  <v-container>Home</v-container>
+  <v-container>
+    <h1>Home</h1>
+    <dir v-if="$apollo.loading">Loading...</dir>
+    <ul v-for="post in getPosts" :key="post._id">
+      <li>
+        {{post.title}}
+        {{post.imageUrl}}
+        {{post.description}}
+      </li>
+      <li>
+        {{post.likes}}
+      </li>
+    </ul>
+  </v-container>
 </template>
 
 <script>
+import { gql } from 'apollo-boost';
+
 export default {
-  name: "home"
+  name: "home",
+  apollo: {
+    getPosts: {
+      query: gql`
+        query {
+          getPosts {
+            _id
+            title
+            imageUrl
+            description
+            likes
+          }
+        }
+      `
+    }
+  },
 };
 </script>
--- client/src/main.js
+++ client/src/main.js
@@ -4,10 +4,22 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import '@babel/polyfill'
+import ApolloClient from 'apollo-boost';
+import VueApollo from 'vue-apollo';
+
+Vue.use(VueApollo);
+
+// setup ApolloClient
+const defaultClient = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
+
+const apolloProvider = new VueApollo({ defaultClient });
 
 Vue.config.productionTip = false
 
 new Vue({
+  provide: apolloProvider.provide(),
   router,
   store,
   vuetify,
