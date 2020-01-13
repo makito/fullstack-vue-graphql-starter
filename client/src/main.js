661b348a0864380ccb73aa31e95319c449f8a826
Ivan Orekhov <i.orehov@mcplat.ru>
2020-01-13 08:42:30 +0300

    refactor for remove deprecation varning

--- client/src/main.js
+++ client/src/main.js
@@ -19,7 +19,7 @@ const apolloProvider = new VueApollo({ defaultClient });
 Vue.config.productionTip = false
 
 new Vue({
-  provide: apolloProvider.provide(),
+  apolloProvider,
   router,
   store,
   vuetify,
